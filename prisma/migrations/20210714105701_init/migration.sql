e5861cc34b4efb6cce52f4a004e5ebbcfa700823
danyu rajbahak <rajbahakdanyu@gmail.com>
2021-07-14 16:42:55 +0545

    Updated model

--- prisma/migrations/20210714105701_init/migration.sql
+++ prisma/migrations/20210714105701_init/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
-CREATE TABLE `User` (
+CREATE TABLE `user` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
     `username` CHAR(255) NOT NULL,
 
-    UNIQUE INDEX `User.username_unique`(`username`),
+    UNIQUE INDEX `user.username_unique`(`username`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
